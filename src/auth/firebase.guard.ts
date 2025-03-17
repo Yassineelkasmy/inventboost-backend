420f07b23aed65e3738081224fffae1aedeb198f
Yassine El Kasmy <yassineelkasmy@gmail.com>
2025-03-17 06:29:52 +0000

    cache user session

--- src/auth/firebase.guard.ts
+++ src/auth/firebase.guard.ts
@@ -1,9 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { FirebaseService } from "./firebase.service";
+import { SessionService } from "./session.service";
 
 @Injectable()
 export class FirebaseAuthGuard implements CanActivate {
-    constructor(private firebaseService: FirebaseService) { }
+    constructor(
+        private firebaseService: FirebaseService,
+        private sessionService: SessionService,
+    ) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
@@ -13,24 +17,31 @@ export class FirebaseAuthGuard implements CanActivate {
     async validateRequest(req: any): Promise<boolean> {
         const token = req.headers.authorization;
 
-        if (token != null && token != '') {
-            try {
-                const decodedToken = await this.firebaseService.defaultApp
-                    .auth()
-                    .verifyIdToken(token.replace('Bearer ', ''));
+
+        let user = await this.sessionService.getSession(token)
+
+        if (!user) {
+            if (token != null && token != '') {
+                try {
+                    const decodedToken = await this.firebaseService.defaultApp
+                        .auth()
+                        .verifyIdToken(token.replace('Bearer ', ''));
 
 
-                const user = {
-                    uid: decodedToken.uid,
-                    email: decodedToken.email,
-                };
-                req.user = user;
-            } catch (_) {
+                    user = {
+                        uid: decodedToken.uid,
+                        email: decodedToken.email,
+                    }
+                    await this.sessionService.setSession(token, user)
+                } catch (_) {
+                    return false;
+                }
+            } else {
                 return false;
             }
-            return true;
-        } else {
-            return false;
         }
+
+        req.user = user;
+        return true;
     }
 }
